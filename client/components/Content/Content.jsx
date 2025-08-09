502b9e7cf1fb03b0b4554cc04479ac817b95ff0e
jaehyung <j43hyun9@gmail.com>
2025-08-09 10:20:13 +0900

    | ✨ | 카카오 API 맵 받아오기 적용

--- client/components/Content/Content.jsx
+++ client/components/Content/Content.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './Content.module.css';
 import Title from '../Title/Title.jsx';
 import TopSix from '../TopSix/TopSix.jsx';
-import Upload from '../Upload.jsx'; // Upload 컴포넌트 import
+import ShelterMap from '../ShelterMap/ShelterMap.jsx'; // ShelterMap 컴포넌트 import
 
 const Content = () => {
   return (
     <div className={styles["content-container"]}>
       <Title />
       <TopSix />
-      <Upload /> {/* Upload 컴포넌트 사용 */}
+      <ShelterMap /> {/* ShelterMap 컴포넌트 사용 */}
     </div>
   );
 };
