6ed7e5cfbb3c21aaddbbffd5f6d738f57ed876c6
leesumin <dltnals0109@naver.com>
2025-08-09 09:37:50 +0900

    Header.css수정

--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useMediaQuery } from 'react-responsive';
-import './Header.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const nav = useNavigate();
@@ -15,14 +15,12 @@ const Header = () => {
     console.log("Login");
     nav("/login");
     setIsMenuOpen(false);
-  }
+  };
   const toggleJoin = () => {
     console.log("Join");
     nav("/register");
     setIsMenuOpen(false);
-  }
-
-  
+  };
 
   return (
     <>
@@ -30,26 +28,37 @@ const Header = () => {
         <div className="logo">
           <a href="/">
             <div className="imgFoot">
-            <img src="/images/foot.png" alt="로고" width="40" />
+              <img src="/images/foot.png" alt="로고" width="40" />
             </div>
-            <span className="logo-text">PAW </span>
-            <span className="logo-text">PAW </span>
-
           </a>
         </div>
-        <div className={`hamburger-menu ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+
+        <div className="center-title">PAWPAW</div>
+
+        <div
+          className={`hamburger-menu ${isMenuOpen ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
           <div className="bar"></div>
           <div className="bar"></div>
           <div className="bar"></div>
         </div>
       </header>
       {/* <div className="header-background"></div> */}
-      <nav className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
-          <ul style={{display:'flex', flexDirection:'column'}}>
-            <li><a href="#" onClick={toggleLogin}>로그인</a></li>
-            <li><a href="#" onClick={toggleJoin}>회원가입</a></li>
-          </ul>
-        </nav>
+      <nav className={`nav-menu ${isMenuOpen ? "active" : ""}`}>
+        <ul style={{ display: "flex", flexDirection: "column" }}>
+          <li>
+            <a href="#" onClick={toggleLogin}>
+              로그인
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={toggleJoin}>
+              회원가입
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
